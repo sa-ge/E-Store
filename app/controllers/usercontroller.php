457b36c7994d5f3b111e46e0e8cebbcca2e46c6d
alhelaly <alhe22lali@gmail.com>
2020-09-02 05:59:35 +0300

    added usercontroller.php file

---
+++ app/controllers/usercontroller.php
@@ -0,0 +1,95 @@
+<?php
+namespace PHPMVC\CONTROLLERS;
+use PHPMVC\LIB\DATABASE\DB;
+use PHPMVC\CONTROLLERS\AbstractController;
+
+
+class EmployeeController extends AbstractController
+{
+    public function defaultAction()
+    {
+        
+        $emps = DB::getInstance()->get('employees',array( 'id' , '>','0'));
+        
+       if(!is_null($emps->results())){ 
+              for($counter = 0; $counter <= count($emps->results()); $counter=$counter +1)
+              {
+                $row = $emps->results();
+                $_SESSION['employees'] = $row;
+              
+              }
+           }
+               $this->_view();
+    }
+
+    public function addAction()
+    {
+        $this->_view();
+    }
+
+    public function editAction()
+    {
+        $emEdit = DB::getInstance()->update('employees', 1 , array(
+            'name' => 'alhelaly',
+            'salary' => '8700'
+        )); 
+        $this->_view();
+
+        
+    }
+    
+    public function storeAction()
+    {
+        static $age;
+        static $name;
+        static $address;
+        static $gender;
+        static $sys = array();
+        static $salary;
+        static $taxRate;
+        static $notes;
+        static $jobtype;
+        if(isset($_POST['submit']))
+        {
+            $name= $_POST['name'];
+            $age = $_POST['age'];
+            $address = $_POST['address'];
+            $gender = $_POST['gender'];
+            $notes = $_POST['notes'];
+            $taxRate = $_POST['tax'];
+            $salary =$_POST['salary'];
+            $sys = $_POST['os'];
+            $jobtype = $_POST['type'];
+        }
+
+        $os = implode(',',$sys);
+        
+        $emp = DB::getInstance()->insert('employees', array(
+
+             'name' => $name,
+             'age' => $age,
+             'gender' => $gender, 
+             'address' => $address, 
+             'systemsCanUse' => $os, 
+             'salary' => $salary, 
+             'taxRate' => $taxRate,
+             'notes' => $notes,
+             'job_type' => $jobtype
+             
+         ));
+         header("Location:default");
+        
+        // $em = DB::getInstance()->get('employees' , array('id' ,'=', '1'));
+        // if(!$em->count()){
+        //     echo "no user";
+        // }
+        // else 
+        // {
+        //     foreach($em->results() as $emp){
+        //         echo $emp->systemsCanUse;
+        //         }
+            
+        // }
+         
+    }
+}
