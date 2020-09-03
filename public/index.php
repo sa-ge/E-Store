d48a5a399ff0a7184af4017076f73eee69bcdd42
alhelaly <alhe22lali@gmail.com>
2020-09-03 20:16:34 +0300

    -

--- public/index.php
+++ public/index.php
@@ -1,19 +1,20 @@
 <?php
 namespace PHPMVC;
-session_start();
+//session_start();
 
 
 if (!defined('DS'))
 {
     define('DS', DIRECTORY_SEPARATOR);
 }
-use PHPMVC\LIB\FrontController;
-
-require_once('..'. DS .'app'.DS.'config.php');
-require_once(APP_PATH . DS.'lib' .DS.'database' . DS .  'init.php');
-require_once(APP_PATH . DS .'lib' . DS . 'autoload.php');
-$FrontController = new FrontController();
-$FrontController->dispatch();
+    
+require_once('register.php');
+//use PHPMVC\LIB\FrontController;
+//require_once('..'. DS .'app'.DS.'config.php');
+//require_once(APP_PATH . DS.'lib' .DS.'database' . DS .  'init.php');
+//require_once(APP_PATH . DS .'lib' . DS . 'autoload.php');
+//$FrontController = new FrontController();
+//$FrontController->dispatch();
 
 
 
