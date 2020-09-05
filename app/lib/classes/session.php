7384d999417756427e8082a18371816c5600e8e4
alhelaly <alhe22lali@gmail.com>
2020-09-05 10:12:24 +0300

    added session.php file
    add session class which contain 4 methods, one to check if value exists in the session variable, the second one is put to create new values in session, the theird one to get value of session varible, the last one to unset value from the session varibale

---
+++ app/lib/classes/session.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace PHPMVC\LIB\CLASSES;
+
+
+
+class Session
+{
+    public static function exists($name)
+    {
+        if((isset($_SESSION[$name])))
+        {
+            return true;
+        }
+            return false;
+    }
+
+    public static function put($name , $value)
+    {
+        return $_SESSION[$name] = $value;
+    }
+    public static function get($name)
+    {
+        return $_SESSION[$name];
+    }
+     
+    public static function delete($name)
+    {
+        if(self::exists($name))
+        {
+            unset($_SESSION[$name]);
+        }
+    }
+    
+    
+        
+}
+
