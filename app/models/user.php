ca71658afe3af61777d2f6ba4bc887bad8c63a1b
alhelaly <alhe22lali@gmail.com>
2020-09-07 03:12:25 +0300

    added user model
    added create method to allow inserting data directly from the controller

---
+++ app/models/user.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PHPMVC\MODELS;
+
+use Exception;
+use PHPMVC\LIB\DATABASE\DB;
+
+class User 
+{
+    private $_db;
+
+       public function __construct($user = null)
+       {
+           $this->_db = DB::getInstance();
+       }
+
+       public function create($fields = array())
+       {
+           if(!$this->_db->insert('users', $fields)) 
+           {
+               throw new Exception('There was a problem creating an account. ');
+           }
+       }
+        
+
+}
