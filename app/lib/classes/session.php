56259d7939805606274c40cfd4e9e31013325bca
alhelaly <alhe22lali@gmail.com>
2020-09-07 21:18:08 +0300

    added setObject function to Session

--- app/lib/classes/session.php
+++ app/lib/classes/session.php
@@ -32,6 +32,32 @@ class Session
         }
     }
     
+    public static function flash($name , $string = '')
+    {
+        if(self::exists($name))
+        {
+            $session = self::get($name); 
+            self::delete($name);
+            return $session;
+        }
+        else
+        {
+            self::put($name , $string);
+        }
+
+    }
+        
+    public static function putObject($name ,$param)
+    {
+        if(self::exists($name)){
+            self::delete($name);
+            self::put($name, $param);
+        }else{
+
+            self::put($name, $param);
+        }
+    }
+    
     
         
 }
