644a9f69a54d13f7d2c046c68434b65ccc73c704
alhelaly <alhe22lali@gmail.com>
2020-09-09 19:07:37 +0300

    -

--- app/views/user/create.view.php
+++ app/views/user/create.view.php
@@ -1,16 +1,32 @@
+<?php
+
+use PHPMVC\LIB\CLASSES\Token;
+use PHPMVC\MODELS\Input;
+use PHPMVC\LIB\FUNCTIONS\Sanitize;
+use PHPMVC\LIB\CLASSES\Session;
+if(Session::exists('errors')){
+    $x = Session::flash('errors');
+    if(is_array($x)){
+        var_dump($x);
 
 
+?>
+<p style="background-color: tomato;" class="message t"><a href="" class="closeBtn"><i class="fa fa-times"></i></a><? echo Session::flash('employee_added_success'); ?></p>
 
-<form autocomplete="off" class="appForm clearfix" method="post" enctype="application/x-www-form-urlencoded" action="check">
+<?}
+    }
+?>
+
+<form autocomplete="off" class="appForm clearfix" method="post" enctype="application/x-www-form-urlencoded" action="create">
     <fieldset>
         <legend>بيانات المستخدم</legend>
         <div class="input_wrapper n60 border">
-        <label <?if(isset($_POST['submit'])){?> class="floated" <?}?>>الاسم </label>
-            <input required type="text" name="name" maxlength="50" <?if(isset($_POST['submit'])) {?>value="<?=$_POST['name'];};?>">
+        <label <?if(!empty(Input::get('name'))){?> class="floated" <?}?>>الاسم </label>
+        <input required type="text" name="name" maxlength="50" value="<?php if(!empty(Input::get('name'))) echo Sanitize::escape(Input::get('name'));?>">
         </div>
         <div class="input_wrapper n40 padding border">
-         <label <?if(isset($_POST['submit'])){?> class="floated" <?}?>>اسم المستخدم</label>
-            <input required type="text" name="user_name" maxlength="20" <? if(isset($_POST['submit'])) {?> value="<?=$_POST['user_name'];};?>">
+         <label <?if(!empty(Input::get('name'))){?> class="floated" <?}?>>اسم المستخدم</label>
+            <input required type="text" name="user_name" maxlength="20" value="<?php if(!empty(Input::get('user_name')))echo Sanitize::escape(Input::get('user_name'));?>">
         </div>
         <div class="input_wrapper n40 border padding">
             <label >كلمة المرور</label>
@@ -23,12 +39,11 @@
         <div class="input_wrapper_other padding n20 select">
             <select required name="GroupId">
                 <option value=""></option>
-                <?php// if (false !== $groups): foreach ($groups as $group): ?>
                     <option value="" selected>آدمن</option>
                     <option value="">المسؤول المالي</option>
-                <?php //endforeach;endif; ?>
             </select>
         </div>
+        <input class="hidden"  name="token" value="<?php echo Token::generate();?>">
         <input class="no_float" type="submit" name="submit" value="حفظ">
     </fieldset>
 </form>
