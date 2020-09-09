deeebd87a9b4e6cd1a56ce0da56f82dea81bb7ad
alhelaly <alhe22lali@gmail.com>
2020-09-09 19:08:23 +0300

    display all users in the default users page

--- app/views/user/default.view.php
+++ app/views/user/default.view.php
@@ -1,28 +1,33 @@
+<?php
+
+use PHPMVC\LIB\CLASSES\Session;
+use PHPMVC\LIB\FUNCTIONS\Sanitize; ?>
 <div class="container">
-    <a href="/user/create" class="button"><i class="fa fa-plus"></i></a>
+    <a href="/user/create" class="button"> إضافة <i class="fa fa-plus"></i></a>
     <table class="data">
         <thead>
             <tr>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
+                <th>م</th>
+                <th>الاســـــــــــم</th>
+                <th>المستخدم</th>
+                <th>تاريخ الاضـافة</th>
+                <th>التحكم</th>
             </tr>
         </thead>
+            <?php $emp = Session::putObject('users'); ?>
         <tbody>
+            <? for($count = 0; $count <count($emp); $count++){?>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+            <td><?php echo Sanitize::escape($emp[$count]->id); ?></td>
+                <td><?php echo Sanitize::escape($emp[$count]->name); ?></td>
+                <td><?php echo Sanitize::escape($emp[$count]->user_name); ?></td>
+                <td><?php echo Sanitize::escape($emp[$count]->joined); ?></td>
                 <td>
-                    <a href="/users/edit/"><i class="fa fa-edit"></i></a>
-                    <a href="/users/delete/" onclick="if(!confirm('')) return false;"><i class="fa fa-trash"></i></a>
+                <a href="/user/edit/<?= Sanitize::escape($emp[$count]->id);?>"><i class="fa fa-edit"></i></a>
+                <a href="/user/delete/<?= Sanitize::escape($emp[$count]->id);?>" onclick="if(!confirm('')) return false;"><i class="fa fa-trash"></i></a>
                 </td>
             </tr>
+            <?}?>
         </tbody>
     </table>
 </div>
