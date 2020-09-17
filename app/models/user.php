8979405470786c991172644793b63a68443abf42
alhelaly <alhe22lali@gmail.com>
2020-09-17 18:07:30 +0300

    insert joined date only the first time user createdOC

--- app/models/user.php
+++ app/models/user.php
@@ -23,6 +23,7 @@ class User
                }
            }
            else{
+                   unset($fields['joined']);
                if(!$this->_db->update('users', $id ,$fields))
                {
                    throw new Exception('There was a problem updating  the account. ');
